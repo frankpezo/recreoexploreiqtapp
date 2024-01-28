61c7baa3917111fe1fa1d3bf1c7439786d28dd44
frankpezo <73926318+frankpezo@users.noreply.github.com>
2024-01-28 02:39:45 -0500

    Splash screen terminado

--- lib/src/pages/splash_screen.dart
+++ lib/src/pages/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recreoexploreiqtapp/src/pages/home_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   SplashScreen({Key? key}) : super(key: key);
@@ -9,7 +10,60 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    var d = const Duration(seconds: 10);
+    Future.delayed(d, () {
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => HomeScreen()),
+        (route) => false,
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        color: Color(0xFF238F8F),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/images/icon2.png",
+                width: 110.0,
+                height: 110.0,
+                fit: BoxFit.contain,
+              ),
+              SizedBox(height: 16.0),
+              RichText(
+                text: TextSpan(
+                  text: 'RecreoExplore',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.normal,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: 'IQT',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 100.0),
+              CircularProgressIndicator(
+                color: Colors.white,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
